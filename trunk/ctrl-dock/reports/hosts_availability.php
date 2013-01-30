f96e111b088b4b0d8414b3715485030f82ac4f6a
msvishwanath@gmail.com <msvishwanath@gmail.com@3949de35-ce60-a4e1-1461-66b4544f0022>
2013-01-30 07:05:58 +0000

    Bug Fixes for Network Availability exceeding 100% in reports

--- trunk/ctrl-dock/reports/hosts_availability.php
+++ trunk/ctrl-dock/reports/hosts_availability.php
@@ -8,7 +8,7 @@ $host_list = load_xml($url);
 	<td class='reportdata' width=100%><b>Hosts / Service Availability</td>
 </tr>
 </table>
-<table class="reporttable" width=100% cellspacing=1 cellpadding=2>
+<table class="reporttable" width=100% cellspacing=1 cellpadding=4>
 <tr>	
 	<td class="reportheader">Description</td>
 	<td class="reportheader">Hostname</td>
@@ -16,7 +16,8 @@ $host_list = load_xml($url);
 	<td class="reportheader">Network Uptime</td>	
 	<td class="reportheader">Network Downtime</td>	
 	<td class="reportheader">Service Uptime</td>	
-	<td class="reportheader">Service Downtime</td>	
+	<td class="reportheader">Service Downtime</td>
+	<td class="reportheader">Service Information</td>		
 
 </tr>
 <?
@@ -33,7 +34,7 @@ if(count($host_list)>0){
 			echo "<td class='reportdata'>".$hostname."</td>";
 			echo "<td class='reportdata' style='text-align: center;'>".$platform."</td>";
 			
-			$url=$base_url."/api/hosts_availability.php?key=$API_KEY&hostname=$hostname&start_date=$start_date&end_date=$end_date";			
+			$url=$base_url."/api/hosts_availability.php?key=$API_KEY&hostname=$hostname&start_date=$start_date&end_date=$end_date";		
 			$host_availability = load_xml($url);
 			
 			$uptime=$host_availability->availability[0]->uptime;
@@ -55,16 +56,16 @@ if(count($host_list)>0){
 			$downtime=$host_availability->availability[0]->downtime;
 			echo "<td class='reportdata' style='text-align: center;'>".$downtime." %</td>";
 			
-			echo "</tr>";
+			
 			
 			// Display Service Information
-			echo "<tr bgcolor=#EEEEEE>";
-			echo "<td colspan=7 class='reportdata'><b>Service Uptime Summary : </b>";
+			
+			echo "<td colspan=1 class='reportdata'>";
 			$url=$base_url."/api/hosts_svc_availability_info.php?key=$API_KEY&hostname=$hostname&start_date=$start_date&end_date=$end_date";	
 			$service_list = load_xml($url);
 			if(count($service_list)>0){
 				for($j=0;$j<count($service_list);$j++){					
-					echo $service_list->service_info[$j]->description ." (". $service_list->service_info[$j]->port. ") &nbsp; : ".$service_list->service_info[$j]->uptime . "% ";
+					echo $service_list->service_info[$j]->description ." (". $service_list->service_info[$j]->port. ") ".$service_list->service_info[$j]->uptime . "%<br>";
 				}
 			}
 			
