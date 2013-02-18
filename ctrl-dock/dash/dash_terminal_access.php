759594f8b4d631b92d277157b971216f3163bf52
msvishwanath <msvishwanath@gmail.com>
2013-02-18 07:34:25 +0000

    Security Fix for Terminal Access

--- ctrl-dock/dash/dash_terminal_access.php
+++ ctrl-dock/dash/dash_terminal_access.php
@@ -21,13 +21,13 @@ var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directori
 
 <table border=0 width=100% cellpadding="2" cellspacing="0" bgcolor=#E5E5E5 >
   <tr>
-    <td width="90%" style="border-style: none; border-width: medium" align=left colspan=1>
+    <td width="100%" style="border-style: none; border-width: medium" align=left colspan=1>
 	<font face=Arial size=2 color=#CC0000><b>TERMINAL ACCESS</b></font>
 	</td>
 	</tr>
 </table>
 <br>
-<table class="reporttable" width=100% cellpadding="0" cellspacing="1">
+<table class="reporttable" width=100% cellpadding="4" cellspacing="1">
 <tr>	
 	<td class="reportheader" >Service</td>
 	<td class="reportheader" >IP Address / Hostname</td>
@@ -39,7 +39,7 @@ $url=$base_url."/api/terminal_access.php?key=$API_KEY&username=$username";
 $terminal_list = load_xml($url);
 
 if(count($terminal_list)>0){
-$j=1;
+	$j=1;
 	for($i=0;$i<count($terminal_list);$i++){	
 		if (($j%2)==1){$row_color="#EDEDE4";}else{$row_color="#FFFFFF";}
 			$hostname=$terminal_list->terminal[$i]->url;
@@ -50,17 +50,25 @@ $j=1;
 			echo "<tr bgcolor=$row_color>";
 			echo "<td class='reportdata'>".$service."</td>";
 			echo "<td class='reportdata'>".$hostname."</td>";
-			echo "<td class='reportdata' width=90>".$servicetype."</td>";			
-			if($service!='')
-			{
-			echo "<td class='reportdata' style='text-align:center;' width=20><a href='javascript:void(0);'  onclick=\"PopupCenter('http://". $_SERVER['SERVER_NAME'] . ":". $TERMINALPORT."/anyterm.html? $serviceuser@$hostname $service $username', 'terminal',1080,768);\"><div class='myimg'><img border=0 src=../images/terminal.png /></div></a></td>";}
-			else
-			{
-			echo "<td class='reportdata' style='text-align: center;'>NA</td>";
+			echo "<td class='reportdata' width=90>".$servicetype."</td>";
+			
+			if($service!=''){
+				echo "<td class='reportdata' style='text-align:center;' width=20><a href='javascript:void(0);' onclick=\"PopupCenter('http://".$_SERVER['SERVER_NAME'].":".$TERMINALPORT."/anyterm.html?$serviceuser@$hostname $service $username', 'terminal',1080,768);\"><div class='myimg'><img border=0 src=../images/terminal.png /></div></a></td>";
+			}else{
+				echo "<td class='reportdata' style='text-align: center;'>NA</td>";
 			}
 			echo "</tr>";
 		}
-	$j++; }
+		$j++; 
+	}
+	
+	$sql="delete from terminal_login where username='$username'";
+	$result = mysql_query($sql);
+	
+	$sql="insert into terminal_login values ('$username')";
+	$result = mysql_query($sql);
 }
 ?>
 </table>
+<meta http-equiv="refresh" content="20">
+
