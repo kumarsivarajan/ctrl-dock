86fe76007d29794e463cd3bc6abcac536dc3c987
msvishwanath@gmail.com <msvishwanath@gmail.com@3949de35-ce60-a4e1-1461-66b4544f0022>
2013-02-18 07:31:08 +0000

    Security Fix for terminal login

--- trunk/ctrl-dock/logout.php
+++ trunk/ctrl-dock/logout.php
@@ -1,7 +1,12 @@
 <?php
-	include("auth.php"); 
+include("auth.php"); 
 
+//Reset terminal login status
 
+$sql="delete from terminal_login where username='".$_SESSION['username']."'";
+$result = mysql_query($sql);
+	
+	
 // if the user is logged in, unset the session
 
 if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
--- trunk/ctrl-dock/terminal/pwdcheck/get_pwd.php
+++ trunk/ctrl-dock/terminal/pwdcheck/get_pwd.php
@@ -11,21 +11,32 @@ $username = $ra_split[0];
 $servicename = $argv[2];
 $portal_id = $argv[3];
 
-
-$sql="select d.password, d.port, d.type from user_master a, user_group b, group_service c,service_properties d";
-$sql.=" where a.username=b.username and b.group_id=c.group_id and c.service=d.service and";
-$sql.=" c.service='$servicename' and a.username='$portal_id'";
+$sql	="select count(*) from terminal_login where username='$portal_id'";
 $result = mysql_query($sql);
-if(mysql_num_rows($result)){
-	$row = mysql_fetch_row($result);
-	$password=$row[0];
-	$port=$row[1];
-	if ($row[2]=='SSH'){
-		if ($port == '' or $port == 0){
-			$port = 22;
+$row 	= mysql_fetch_row($result);
+$verify=$row[0];
+
+if ($verify==1){
+
+	// Reset Login status
+	$sql="delete from terminal_login where username='$portal_id'";
+	$result = mysql_query($sql);
+	
+	$sql="select d.password, d.port, d.type from user_master a, user_group b, group_service c,service_properties d";
+	$sql.=" where a.username=b.username and b.group_id=c.group_id and c.service=d.service and";
+	$sql.=" c.service='$servicename' and a.username='$portal_id'";
+	$result = mysql_query($sql);
+	if(mysql_num_rows($result)){
+		$row = mysql_fetch_row($result);
+		$password=$row[0];
+		$port=$row[1];
+		if ($row[2]=='SSH'){
+			if ($port == '' or $port == 0){
+				$port = 22;
+			}
 		}
+		$command="expect $installation_path/terminal/scripts/myssh.exp $argv[1] '$password' $username $port";
+		passthru($command);
 	}
-	$command="expect $installation_path/terminal/scripts/myssh.exp $argv[1] '$password' $username $port";
-	passthru($command);
 }
 ?>
