e7fc97f3b93ac9dfa21feca48b0b8c2b5d1f90ad
msvishwanath@gmail.com <msvishwanath@gmail.com@3949de35-ce60-a4e1-1461-66b4544f0022>
2013-04-01 12:33:58 +0000

    Schema Fixes

--- trunk/ctrl-dock/db_schema/rim.sql
+++ trunk/ctrl-dock/db_schema/rim.sql
@@ -1609,7 +1609,7 @@ CREATE TABLE `poa_approval_history` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 CREATE TABLE `terminal_login` (
-  `username` varchar(255) default NULL;
+  `username` varchar(255) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
