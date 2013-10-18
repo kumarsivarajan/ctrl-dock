18cbaaf6ed6a9bae0c342bdf1f4e59b70d38e9c0
msvishwanath@gmail.com <msvishwanath@gmail.com@3949de35-ce60-a4e1-1461-66b4544f0022>
2013-10-18 05:33:06 +0000

    Bug fix for top banner on Internet Explorer.

--- trunk/ctrl-dock/frames.php
+++ trunk/ctrl-dock/frames.php
@@ -5,7 +5,7 @@
 <center>
 	<table border="0" width="1024" height="100%" cellspacing="0" cellpadding="0" bgcolor=#FFFFFF>
 		<tr>
-			<td height="40"><iframe name="rim_menu" src="menu.php" height=100% width=100% scrolling="no" border="0" frameborder="0" target="rimmain"></iframe></td>
+			<td height="40"><iframe name="rim_menu" src="menu.php" height=40 width=100% scrolling="no" border="0" frameborder="0" target="rimmain"></iframe></td>
 		</tr>
 		<tr>
 		<?if($DASH==1){?>
